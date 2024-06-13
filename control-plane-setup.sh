66b5394da8e875e7fbc50aa764577c928d8bf756
Mostafa Kamal <hiremostafa@gmail.com>
2024-06-14 01:36:39 +0600

    refactor

--- control-plane-setup.sh
+++ control-plane-setup.sh
@@ -26,11 +26,11 @@ fi
 
 # Run containerd installation
 print_green "Running containerd installation..."
-#./common/containerd-install.sh
+./common/containerd-install.sh
 
 # Run Kubernetes installation
 print_green "Running Kubernetes installation..."
-#./common/k8s-install.sh
+./common/k8s-install.sh
 
 # Fetch the private IP address of the VM
 private_ip=$(hostname -I | awk '{print $1}')
@@ -38,30 +38,18 @@ private_ip=$(hostname -I | awk '{print $1}')
 # Specify the port for the API server
 api_server_port=6443
 
-# Create the configuration file with the actual values
-config_file="config/kubeadm-custom-config.yml"
-mkdir -p config
-cat <<EOF > $config_file
-# config/kubeadm-custom-config.yml
-apiVersion: kubeadm.k8s.io/v1beta3
-kind: ClusterConfiguration
-kubernetesVersion: stable
-controlPlaneEndpoint: "${public_ip}:${api_server_port}"
-networking:
-  podSubnet: "${pod_network_cidr}"
-apiServer:
-  certSANs:
-    - "${public_ip}"
-    - "${private_ip}"
----
-apiVersion: kubelet.config.k8s.io/v1beta1
-kind: KubeletConfiguration
-cgroupDriver: systemd
-EOF
+# Define the path for the config file
+kubeadm_config_file="config/kubeadm-custom-config.yml"
+kubeadm_generated_config_file="config/actual-kubeadm-custom-config.yml"
+
+print_green "Generate custom kubeadm config"
+# Read the contents of kubeadm_config_file, substitute variables, and write to the generated config file
+eval "echo \"$(cat $kubeadm_config_file)\"" > $kubeadm_generated_config_file
+
 
 # Initialize the Kubernetes control plane
 print_green "Initializing the Kubernetes control plane (via: kubeadm init)..."
-sudo kubeadm init --config="$config_file" --upload-certs
+sudo kubeadm init --config="$kubeadm_generated_config_file" --upload-certs
 
 # Set up kubeconfig for the root user
 print_green "Setting up kubeconfig for the root user..."
